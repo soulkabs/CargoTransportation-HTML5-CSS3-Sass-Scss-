01be9b87ee0f9aea67bc57626c9c2d501ca6db9f
soulkabs <52444722+soulkabs@users.noreply.github.com>
2019-11-22 12:05:30 +0300

    BEM and jQuery script

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -7,20 +7,11 @@ function plusSlides(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slide");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   slides[slideIndex-1].style.display = "flex";  
-}
-
-function showInfo() {
-  var info = document.getElementById("servicesInfo");
-  if (info.style.display === "block") {
-    info.style.display = "none";
-  } else {
-    info.style.display = "block";
-    }
 }
